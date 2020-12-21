8a0d1b01e9806b44381df64494f9035f704725ff
Mike Kaufman <mikekaufman4@gmail.com>
2020-12-20 23:16:42 -0800

    IC

--- src/chunks.js
+++ src/chunks.js
@@ -1,8 +1,9 @@
-const {getIdList, fetchPhoto, chunks} = require('./helpers');
+const { getIdList, fetchPhoto, chunks } = require('./helpers');
 
 const t0 = Date.now();
-chunks(getIdList(2000), fetchPhoto, 50)
+chunks(getIdList(150), fetchPhoto, 50)
   .then(res => {
     const t1 = Date.now();
+    console.log(res)
     console.log(`Fetch time: ${t1 - t0} ms`);
   });
--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,40 @@
-const axios = require('axios');
+// https://dev.to/karataev/handling-a-lot-of-requests-in-javascript-with-promises-1kbb
 
+const request = require('request');
 const getIdList = n => [...new Array(n)].map((item, i) => i + 1);
 
-const fetchPhoto = id => {
+const promisifiedRequest = function (options) {
+  return new Promise((resolve, reject) => {
+    try {
+      request(options, (error, response, body) => {
+        if (response) {
+          return resolve(response);
+        }
+        if (error) {
+          return reject(error);
+        }
+      });
+    }
+    catch {
+      console.log("failed in promisifiedRequest")
+    }
+  });
+};
+
+
+async function fetchPhoto(id) {
   const url = `https://jsonplaceholder.typicode.com/photos/${id}`;
-  return axios.get(url)
-    .then(res => res.data)
+  const options = {
+    url: url,
+    proxy: "http://" + "167.71.194.112:8080",
+    method: 'GET',
+    gzip: true,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36'
+    }
+  };
+  let response = await promisifiedRequest(options);
+  return response.body;
 };
 
 function all(items, fn) {
@@ -26,7 +55,7 @@ function series(items, fn) {
 
 function splitToChunks(items, chunkSize = 50) {
   const result = [];
-  for (let i = 0; i < items.length; i+= chunkSize) {
+  for (let i = 0; i < items.length; i += chunkSize) {
     result.push(items.slice(i, i + chunkSize));
   }
   return result;
